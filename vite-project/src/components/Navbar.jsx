9cc0ec6301a68cacc7a32018269308c1d96f0608
Naman7564 <Naman7564@gmail.com>
2026-01-14 14:52:00 +0530

    responsive design

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbar">
       <div className="logo">
         <img src="/images/logo.png" alt="Ben 10 Logo" />
       </div>
 
-      <ul className="nav-links">
-        <li>Home</li>
-        <li>Aliens</li>
-        <li>Episodes</li>
-        <li>Games</li>
-        <li>About</li>
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li onClick={() => setMenuOpen(false)}>Home</li>
+        <li onClick={() => setMenuOpen(false)}>Aliens</li>
+        <li onClick={() => setMenuOpen(false)}>Episodes</li>
+        <li onClick={() => setMenuOpen(false)}>Games</li>
+        <li onClick={() => setMenuOpen(false)}>About</li>
       </ul>
 
       <div className="nav-buttons">
         <button className="btn btn-outline">Login</button>
         <button className="btn btn-fill">Sign Up</button>
       </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
+
